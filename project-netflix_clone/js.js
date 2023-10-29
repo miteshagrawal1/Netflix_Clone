27afa5f2d4e69c1dcf7e775688d6a0052a612f6a
miteshagrawal1 <115792308+miteshagrawal1@users.noreply.github.com>
2023-10-29 12:00:26 +0530

    Add files via upload

---
+++ project-netflix_clone/js.js
@@ -0,0 +1,50 @@
+console.log("Jai Bhole");
+let que1 = document.getElementById('que1');
+let ans1 = document.getElementById('ans1');
+let que2 = document.getElementById('que2');
+let ans2 = document.getElementById('ans2');
+let que3 = document.getElementById('que3');
+let ans3 = document.getElementById('ans3');
+let que4 = document.getElementById('que4');
+let ans4 = document.getElementById('ans4');
+let que5 = document.getElementById('que5');
+let ans5 = document.getElementById('ans5');
+let que6 = document.getElementById('que6');
+let ans6 = document.getElementById('ans6');
+
+que1.addEventListener("click", () => {
+    ans1.style.display = "block";
+})
+que1.addEventListener("dblclick", () => {
+    ans1.style.display = "none";
+})
+que2.addEventListener("click", () => {
+    ans2.style.display = "block";
+})
+que2.addEventListener("dblclick", () => {
+    ans2.style.display = "none";
+})
+que3.addEventListener("click", () => {
+    ans3.style.display = "block";
+})
+que3.addEventListener("dblclick", () => {
+    ans3.style.display = "none";
+})
+que4.addEventListener("click", () => {
+    ans4.style.display = "block";
+})
+que4.addEventListener("dblclick", () => {
+    ans4.style.display = "none";
+})
+que5.addEventListener("click", () => {
+    ans5.style.display = "block";
+})
+que5.addEventListener("dblclick", () => {
+    ans5.style.display = "none";
+})
+que6.addEventListener("click", () => {
+    ans6.style.display = "block";
+})
+que6.addEventListener("dblclick", () => {
+    ans6.style.display = "none";
+})
